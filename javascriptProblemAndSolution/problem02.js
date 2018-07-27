a1bd4d4ae4a8cc22967146726d34a57309052d19
DESKTOP-4OOTLQN\kjhmd <kjhmd9726@gmail.com>
2018-07-27 23:29:21 +0900

    [Update] Update Javascript solution.

--- javascriptProblemAndSolution/problem02.js
+++ javascriptProblemAndSolution/problem02.js
@@ -24,3 +24,23 @@ function abc() {
 };
 
 abc();
+
+
+
+/***
+ * answer
+ *      1. bbb
+ *      2. undefined
+ *      3. undefined
+ *      4. ccc
+ *      5. ddd
+ * 
+ * Hoisting
+ *      스코프별로 변수 table을 선언 만든다. 스코프 안에 변수 선언 혹은 함수 선언문을 찾아서 변수 테이블에 추가. 대입은 실행 시점에 대입.
+ *      var a = 2; 의 동작 방식
+ *          var a; a = 2;
+ *          변수 table을 만들고 table에 a라는 변수를 추가(인터프리터 시점). 그리고 a에 2를 대입한다(실행 시점).
+ * 
+ *     foo = function()도 실행 시점에 대입.
+ *      변수 선언없이 변수에 값대입을 하면 global table에 변수를 추가하고 대입.
+ */
--- javascriptProblemAndSolution/problem03.js
+++ javascriptProblemAndSolution/problem03.js
@@ -13,3 +13,23 @@ var obj = {
 
 obj.a;
 obj.fn();
+
+
+
+/***
+ * answer: 2(obj), 1(global), 3(global)
+ * 
+ * function fn() { console.log(this) } fn();은 global.(암기)
+ * 
+ * 인터프리터 시점에 변수 table을 만들고 기본적으로 this와 argument변수 추가.
+ * 
+ * var obj = { a: console.log(this) }는 obj.a = console.log(this)와 같음.
+ * 
+ * setTimeout(obj.fn, 100);
+ * 		var temp = obj.fn();
+ * 		setTimeout(temp, 100);으로 변환하고 실행.
+ * 		temp()에서 temp앞에 아무것도 없으므로 this는 global.
+ * 
+ * this를 바꾸는 방법
+ * 		call, apply, bind
+ */
